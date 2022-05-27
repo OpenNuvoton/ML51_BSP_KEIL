9cad92592d70a846933290a09b5251a6cbac5730
MB20 LCWang0 <LCWANG0@nuvoton.com>
2022-05-27 13:55:46 +0800

    Added ISP UART3 project

--- SampleCode/ISP/ISP_UART3/isp_uart3.c
+++ SampleCode/ISP/ISP_UART3/isp_uart3.c
@@ -5,11 +5,12 @@
 /*---------------------------------------------------------------------------------------------------------*/
 
 //***********************************************************************************************************
-//  File Function: ML51 UART0 ISP subroutine
+//  File Function: ML51 UART3 ISP subroutine
 //***********************************************************************************************************
 #include "ML51.h"
-#include "isp_uart0.h"
-
+#include "isp_uart3.h"
+#define SYS_CLK 24000000
+#define uart_baudrate 115200
 xdata volatile uint8_t uart_rcvbuf[64];
 xdata volatile uint8_t uart_txbuf[64];
 data volatile uint8_t bufhead;
@@ -24,13 +25,14 @@ bit volatile bUartDataReady;
 bit volatile g_timer0Over;
 bit volatile g_timer1Over;
 bit volatile g_programflag;
-bit  BIT_TMP;
+
 unsigned char PID_highB, PID_lowB, DID_highB, DID_lowB, CONF0, CONF1, CONF2, CONF4;
 unsigned char recv_CONF0, recv_CONF1, recv_CONF2, recv_CONF4;
 
+
 void READ_ID(void)
 {
-//    set_CHPCON_IAPEN;
+    //    set_CHPCON_IAPEN;
     IAPCN = READ_DID;
     IAPAH = 0x00;
     IAPAL = 0x00;
@@ -49,8 +51,8 @@ void READ_ID(void)
 void READ_CONFIG(void)
 {
     IAPCN = BYTE_READ_CONFIG;
-    IAPAL = 0x00;
     IAPAH = 0x00;
+    IAPAL = 0x00;
     set_IAPTRG_IAPGO;
     CONF0 = IAPFD;
     IAPAL = 0x01;
@@ -62,55 +64,40 @@ void READ_CONFIG(void)
     IAPAL = 0x04;
     set_IAPTRG_IAPGO;
     CONF4 = IAPFD;
-//    clr_CHPCON_IAPEN;
+    //    clr_CHPCON_IAPEN;
 }
 
 void TM0_ini(void)
 {
     TH0 = TL0 = 0;     //interrupt timer 140us
     set_TCON_TR0;      //Start timer0
-    set_IPH_PSH;       // Serial port 0 interrupt level2
+    //set_IPH_PSH;       // Serial port 0 interrupt level2
     set_IE_ET0;
 }
 
 
-void UART0_ini_115200_24MHz(void)
+void UART3_ini_115200(void)
 {
-    MFP_P31_UART0_TXD;                              /* set P2.3 and P2.2 as Quasi mode for UART0 trasnfer */
-    MFP_P30_UART0_RXD;
-    P31_QUASI_MODE;
-    P30_QUASI_MODE;
-  
-    SFRS = 0x00;
-    SCON = 0x50;            /*UART0 Mode1,REN=1,TI=1*/
-    set_PCON_SMOD;          /*UART0 Double Rate Enable*/
-    T3CON &= 0xF8;           /*T3PS2=0,T3PS1=0,T3PS0=0(Prescale=1)*/
-    set_T3CON_BRCK;          /*UART0 baud rate clock source = Timer3*/
-    RH3    = 0xFF;   /* HIBYTE(65536 - 13)*/
-    RL3    = 0xF3;   /* LOBYTE(65536 - 13); */
-    set_T3CON_TR3;          /*Trigger Timer3*/
-    ES=1;
-    EA=1;
+    MFP_P12_UART3_TXD;
+    P12_QUASI_MODE;
+    MFP_P11_UART3_RXD;
+    P11_INPUT_MODE;
+
+    set_SC1CR0_SCEN;         /* Enable SC module */
+    set_SC1CR1_UARTEN;       /* set as UART mode */
+    set_SC1CR1_CLKKEEP;
+    clr_SC1CR0_CONSEL;       /* clr CONSEL and AUTOCEN*/
+    clr_SC1CR0_AUTOCEN;
+    SC1ETURD0 = LOBYTE(SYS_CLK / 8 / uart_baudrate - 1); /* define baud rate low byte */
+    SC1ETURD1 &= 0xF0;                                /* define baud rate high byte */
+    SC1ETURD1 |= (HIBYTE(SYS_CLK / 8 / uart_baudrate - 1)) & 0x0F;
+    set_SC1CR1_PBOFF;     //parity bit disable
+    SC1CR1 &= 0xCF;      //datalegth 8bit
+    set_SC1CR0_NSB;      //stop bit = 1bit
+    ENABLE_SC1_RECEIVE_DATA_REACH_INTERRUPT;
+    ENABLE_GLOBAL_INTERRUPT;
 }
 
-void UART0_ini_115200_7MHz(void)
-{
-    MFP_P31_UART0_TXD;                              /* set P2.3 and P2.2 as Quasi mode for UART0 trasnfer */
-    MFP_P30_UART0_RXD;
-    P31_QUASI_MODE;
-    P30_QUASI_MODE;
-  
-    SFRS = 0x00;
-    SCON = 0x50;            /*UART0 Mode1,REN=1,TI=1*/
-    set_PCON_SMOD;          /*UART0 Double Rate Enable*/
-    T3CON &= 0xF8;           /*T3PS2=0,T3PS1=0,T3PS0=0(Prescale=1)*/
-    set_T3CON_BRCK;          /*UART0 baud rate clock source = Timer3*/
-    RH3    = 0xFF;   /* HIBYTE(65536 - 13)*/
-    RL3    = 0xFC;   /* LOBYTE(65536 - 13); */
-    set_T3CON_TR3;          /*Trigger Timer3*/
-    ES=1;
-    EA=1;
-}
 
 void Package_checksum(void)
 {
@@ -120,7 +107,6 @@ void Package_checksum(void)
     {
         g_checksum = g_checksum + uart_rcvbuf[count];
     }
-
     uart_txbuf[0] = g_checksum & 0xff;
     uart_txbuf[1] = (g_checksum >> 8) & 0xff;
     uart_txbuf[4] = uart_rcvbuf[4] + 1;
@@ -128,56 +114,53 @@ void Package_checksum(void)
 
     if (uart_txbuf[4] == 0x00)
         uart_txbuf[5]++;
-
 }
 
 
-void Send_64byte_To_UART0(void)
+void Send_64byte_To_UART3(void)
 {
-_push_(SFRS);
-    SFRS = 0;
     for (count = 0; count < 64; count++)
     {
-        TI = 0;
-        SBUF = uart_txbuf[count];
-        set_WDCON_WDCLR;
-        while (TI == 0);
+        clr_SC1CR0_TXOFF;
+        SC1DR = uart_txbuf[count];
+
+        while ((SC1TSR | CLR_BIT3) == CLR_BIT3);
+
+        clr_SC1CR0_TXOFF;
     }
-_pop_(SFRS);
 }
 
-void Serial_ISR(void) interrupt 4
+void SMC1_ISR(void) interrupt 27          // Vector @  0xDB
 {
-  SFRS=0;
-    if (RI == 1)
-    {   
-      uart_rcvbuf[bufhead++]=  SBUF;    
-      clr_SCON_RI;                                         // Clear RI (Receive Interrupt).
-    }
-    if (TI == 1)
-    {       
-        clr_SCON_TI;                                       // Clear TI (Transmit Interrupt).
-    }
-    if(bufhead ==1)
+    _push_(SFRS);
+    SFRS = 2;
+
+    if ((SC1IS | CLR_BIT0) == 0xFF)
     {
-      g_timer1Over=0;
-      g_timer1Counter=90;                                  //for check uart timeout using
+        uart_rcvbuf[bufhead++] =  SC1DR;
+        clr_SC1IS_RDAIF;                                         // Clear RI (Receive Interrupt).
     }
-  if(bufhead == 64)
+
+    if (bufhead == 1)
     {
-      
-      bUartDataReady = TRUE;
-      g_timer1Counter=0;
-      g_timer1Over=0;
-      bufhead = 0;
-    }    
+        g_timer1Over = 0;
+        g_timer1Counter = 90; //for check uart timeout using
+    }
+
+    if (bufhead == 64)
+    {
+        bUartDataReady = TRUE;
+        g_timer1Counter = 0;
+        g_timer1Over = 0;
+        bufhead = 0;
+    }
+    _pop_(SFRS);
 }
 
 void Timer0_ISR(void) interrupt 1
 {
-    _push_(SFRS);
-//    if (g_timer0Counter)
-    if (!g_timer0Over)
+        _push_(SFRS);
+    if (g_timer0Counter)
     {
         g_timer0Counter--;
         if (!g_timer0Counter)
@@ -185,6 +168,7 @@ void Timer0_ISR(void) interrupt 1
             g_timer0Over = 1;
         }
     }
+
     if (g_timer1Counter)
     {
         g_timer1Counter--;
@@ -193,5 +177,5 @@ void Timer0_ISR(void) interrupt 1
             g_timer1Over = 1;
         }
     }
-    _pop_(SFRS);
+        _pop_(SFRS);
 }
